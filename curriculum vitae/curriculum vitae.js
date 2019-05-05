8a7766cbd1219b2f34f260e5dceaf049c6c3d00f
hangfeng Wu <nicehangfeng@foxmail.com>
2019-05-05 20:54:28 +0800

    Update curriculum vitae.js

--- curriculum vitae/curriculum vitae.js
+++ curriculum vitae/curriculum vitae.js
@@ -12,18 +12,26 @@ $(function() {
 });
 $(function() {
   var index = 0;
+  var time = 0;
   $(".change_btn .next").click(function() {
     if (index < 3) {
       index++;
     } else {
       index = 0;
     }
-    $('.highlight_tip span').eq(index).addClass('current').siblings().removeClass('current');
+    $(".highlight_tip span")
+      .eq(index)
+      .addClass("current")
+      .siblings()
+      .removeClass("current");
     $(".v_content_list ul li")
       .eq(index)
-      .show().css('opacity',1)
-      .siblings().css('opacity',0)
+      .show()
+      .css("opacity", 1)
+      .siblings()
+      .css("opacity", 0)
       .hide();
+    time = index;
   });
   $(".change_btn .prev").click(function() {
     if (index == 0) {
@@ -31,12 +39,69 @@ $(function() {
     } else {
       index--;
     }
-    $('.highlight_tip span').eq(index).addClass('current').siblings().removeClass('current');
+    $(".highlight_tip span")
+      .eq(index)
+      .addClass("current")
+      .siblings()
+      .removeClass("current");
 
     $(".v_content_list ul li")
       .eq(index)
-      .show().css('opacity',1)
-      .siblings().css('opacity',0)
+      .show()
+      .css("opacity", 1)
+      .siblings()
+      .css("opacity", 0)
+      .hide();
+    time = index;
+  });
+  $(".highlight_tip span").click(function() {
+    $(this)
+      .addClass("current")
+      .siblings()
+      .removeClass("current");
+    $(".v_content_list ul li")
+      .eq($(this).attr("date-index"))
+      .show()
+      .css("opacity", 1)
+      .siblings()
+      .css("opacity", 0)
+      .hide();
+    time = $(this).attr("date-index");
+  });
+
+  setInterval(function() {
+    $(".highlight_tip span")
+      .eq(time)
+      .addClass("current")
+      .siblings()
+      .removeClass("current");
+    $(".v_content_list ul li")
+      .eq(time)
+      .show()
+      .css("opacity", 1)
+      .siblings()
+      .css("opacity", 0)
+      .hide();
+    time++;
+    if (time == 4) {
+      time = 0;
+    }
+  }, 1000);
+});
+$(function() {
+  $(".label").click(function() {
+    $(this)
+      .addClass("label-style")
+      .siblings()
+      .removeClass("label-style");
+    $("section")
+      .eq(
+        $(this)
+          .addClass("#section")
+          .attr("date-item")
+      )
+      .show()
+      .siblings()
       .hide();
   });
 });
